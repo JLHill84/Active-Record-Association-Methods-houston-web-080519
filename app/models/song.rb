c4268ba9e562e1d385178167de4df946055529a7
Joshua Hill <josh@jlhworkshop.com>
2019-08-16 16:51:02 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,6 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   
-  def initialize(title, artist)
-    self.title = title
-    self.artist = artist
-  end
 
   def get_genre_name
     self.genre.name
@@ -15,6 +11,8 @@ class Song < ActiveRecord::Base
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
+    unless self.artist.include?(drake) do
+      self.artist = drake
 
   end
 end
